abb90c73fbfd3dbb048d6f6667aed04517bb0ae2
Nathan Cahill <nathan@nathancahill.com>
2020-03-25 15:17:46 +0100

    move dockerfile to dockerhub

--- postgrest/build.sh
+++ postgrest/build.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-yum install -y perl make automake gcc gmp-devel libffi zlib xz tar gzip git gnupg postgresql-devel
-curl -sSL https://get.haskellstack.org/ | sh
-
 # Output directories
 mkdir $GITHUB_WORKSPACE/postgrest/bin
 mkdir $GITHUB_WORKSPACE/postgrest/lib
 
-# Build from source to /bin
-git clone https://github.com/PostgREST/postgrest.git /tmp/postgrest
-cd /tmp/postgrest
-mkdir $HOME/.stack
-stack build --install-ghc --copy-bins --local-bin-path $GITHUB_WORKSPACE/postgrest/bin
+# Copy binaries
+cp /tmp/postgrest/bin/postgrest $GITHUB_WORKSPACE/postgrest/bin
 
 # Copy required libs to /lib
 cp /usr/lib64/libpq.so* $GITHUB_WORKSPACE/postgrest/lib
@@ -20,6 +14,7 @@ cp /usr/lib64/libecpg_compat.so* $GITHUB_WORKSPACE/postgrest/lib
 cp /usr/lib64/libldap_r* $GITHUB_WORKSPACE/postgrest/lib
 cp /usr/lib64/liblber* $GITHUB_WORKSPACE/postgrest/lib
 cp /usr/lib64/libsasl2* $GITHUB_WORKSPACE/postgrest/lib
+cp /usr/lib64/libssl3* $GITHUB_WORKSPACE/postgrest/lib
 
 # Create tarball
 cd $GITHUB_WORKSPACE/postgrest
