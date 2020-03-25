a9db987cd46fde68879bbf308393d9970bc3d4a5
Nathan Cahill <nathan@nathancahill.com>
2020-03-25 13:24:18 +0100

    add missing libs

--- postgrest/build.sh
+++ postgrest/build.sh
@@ -18,6 +18,7 @@ cp /usr/lib64/libpgtypes.so* $GITHUB_WORKSPACE/postgrest/lib
 cp /usr/lib64/libecpg.so* $GITHUB_WORKSPACE/postgrest/lib
 cp /usr/lib64/libecpg_compat.so* $GITHUB_WORKSPACE/postgrest/lib
 cp /usr/lib64/libldap_r* $GITHUB_WORKSPACE/postgrest/lib
+cp /usr/lib64/liblber* $GITHUB_WORKSPACE/postgrest/lib
 
 # Create tarball
 cd $GITHUB_WORKSPACE/postgrest
